6548a1e12744f4f78c7dd53d4531aa7b5a6bdaaa
Swaroop Gupta Voleti <vswaroop@athenahealth.com>
2020-10-07 14:23:45 +0530

    updated header

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import logo from './logo.png'
+
 export default class home extends Component {
   render() {
     return (
       <div className="wrapper">
         <div className="main-header">
-          <a className="site-logo" href="/">SGV</a>
+          <a className="site-logo" target="_blank" href="https://voletiswaroop.github.io">
+            <img src={logo} alt="Swaroop Gupta Voleti" />
+          </a>
           <div className="navigation-menu">
             <Link to='/ecommerce'>Ecommerce |</Link>
             <Link to='/reports'>Reports |</Link>
